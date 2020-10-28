33c6acc41f217ed1edd9777a11c7d8b2fc993d63
ADITYA108VS <63699699+ADITYA108VS@users.noreply.github.com>
2020-10-28 14:29:54 +0530

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -1,31 +1,41 @@
-#this is made by ADITYA VIKRAM SINHA 11916214  K19PG 
-#i have explained everything here using comments 
-
-
-
 # importing the tkinter module
 from tkinter import *
+
+
+
 
 # importing the pyperclip module to use it to copy our generated 
 # password to clipboard
 import pyperclip
 
+
+
 # random module will be used in generating the random password
 import random
+
 
 # initializing the tkinter
 root = Tk()
 
-# setting the width and height of the gui
+
+# setting the width and height of the gui and color
 root.geometry("900x400")    # x is small case here
+root.configure(bg='#AFF2DE')
+
+
+
 
 # declaring a variable of string type and this variable will be 
 # used to store the password generated
 passstr = StringVar()
 
+
+
 # declaring a variable of integer type which will be used to 
 # store the length of the password entered by the user
 passlen = IntVar()
+
+
 
 # setting the length of the password to zero initially
 passlen.set(0)
@@ -42,42 +52,55 @@ def generate():
             'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
             'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', 
             '9', '0', ' ', '!', '@', '#', '$', '%', '^', '&', 
-            '*', '(', ')']
+            '*', '(', ')','~','&',]
 
     # declaring the empty string
     password = ""
+    
+    
 
     # loop to generate the random password of the length entered           
     # by the user
     for x in range(passlen.get()):
         password = password + random.choice(pass1)
 
+
     # setting the password to the entry widget
     passstr.set(password)
+    
+    
 
 # function to copy the password to the clipboard
 def copytoclipboard():
     random_password = passstr.get()
     pyperclip.copy(random_password)
+    
 
 # Creating a text label widget
-Label(root, text="Password Generator Application  Made By Aditya", font="calibri 20 bold").pack()
+Label(root,fg='black',bg='#AFF2DE',text="Password Generator Application  Made By Aditya", font="arial 22 bold italic").pack()
+
 
 # Creating a text label widget
-Label(root, text="Enter desired password length").pack(pady=3)
+Label(root, fg='black',bg='#AFF2DE',text="Enter your desired password length",font='arial 15 bold').pack(pady=9)
+
 
 # Creating a entry widget to take password length entered by the 
 # user
 Entry(root, textvariable=passlen).pack(pady=3)
 
+
+
 # button to call the generate function
-Button(root, text="Generate Password", command=generate).pack(pady=7)
+Button(root,text="Generate Password",bg='#47FF53',fg='black',font='arial 14 bold', command=generate).pack(pady=7)
+
 
 # entry widget to show the generated password
 Entry(root, textvariable=passstr).pack(pady=3)
 
+
 # button to call the copytoclipboard function
-Button(root, text="Copy to clipboard", command=copytoclipboard).pack()
+Button(root,bg='yellow', text="Copy to clipboard",font='arial 14 bold', command=copytoclipboard).pack()
+
 
 # mainloop() is an infinite loop used to run the application when 
 # it's in ready state 
